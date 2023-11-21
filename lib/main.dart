486ff830eaf6fd61c50eb6dc739afe124ed6dac3
loikmen <loikmen22@gmail.com>
2023-11-21 09:18:13 +0100

    přidány všechny stránky první fáze appky

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,36 @@
 import 'package:flutter/material.dart';
-
 import 'views/LandingPage.dart';
 
 void main() async {
-
   WidgetsFlutterBinding.ensureInitialized();
-  runApp(const Mec3MobileFluttter());
-
+  runApp(const Mec3MobileFlutter());
 }
 
-class Mec3MobileFluttter extends StatelessWidget {
-  const Mec3MobileFluttter({super.key});
+class Mec3MobileFlutter extends StatelessWidget {
+  const Mec3MobileFlutter({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    // Define a material color using Color(0xFFA3AE03)
+    MaterialColor primarySwatchColor = MaterialColor(0xFFA3AE03, {
+      50: Color(0xFFA3AE03),
+      100: Color(0xFFA3AE03),
+      200: Color(0xFFA3AE03),
+      300: Color(0xFFA3AE03),
+      400: Color(0xFFA3AE03),
+      500: Color(0xFFA3AE03),
+      600: Color(0xFFA3AE03),
+      700: Color(0xFFA3AE03),
+      800: Color(0xFFA3AE03),
+      900: Color(0xFFA3AE03),
+    });
+
     return MaterialApp(
       title: 'Mec3',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: primarySwatchColor,
       ),
       home: LandingPage(),
     );
   }
 }
-
-
-
-
--- lib/views/RegisterPage.dart
+++ lib/views/RegisterPage.dart
@@ -2,16 +2,23 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:mec3mobileflutter/views/LandingPage.dart';
-import 'package:mec3mobileflutter/views/RegisterPage.dart';
 
-class LoginPage extends StatelessWidget {
+class RegisterPage extends StatelessWidget {
   final TextEditingController usernameController = TextEditingController();
   final TextEditingController passwordController = TextEditingController();
+  final TextEditingController organizationController = TextEditingController();
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController telNumberController = TextEditingController();
+  final TextEditingController customerNameController = TextEditingController();
 
-  Future<void> loginUser(BuildContext context) async {
-    final String url = 'https://mec3.cz/mec3mobile/UserMobile/LoginUser';
+  Future<void> registerUser(BuildContext context) async {
+    final String url = 'https://mec3.cz/mec3mobile/UserMobile/CreateUser';
     final Map<String, String> headers = {'Content-Type': 'application/json'};
-    final Map<String, String> body = {
+    final Map<String, dynamic> body = {
+      'OrganizationName': organizationController.text,
+      'CustomerName': customerNameController.text,
+      'Email': emailController.text,
+      'TelNumber': telNumberController.text,
       'UserName': usernameController.text,
       'Password': passwordController.text,
     };
@@ -23,44 +30,25 @@ class LoginPage extends StatelessWidget {
     );
 
     if (response.statusCode == 200) {
-      final Map<String, dynamic> responseData = jsonDecode(response.body);
-
-      if (responseData['customerId'] != null) {
-        // Login successful, show success snackbar and navigate to LandingPage
-        ScaffoldMessenger.of(context).showSnackBar(
-          SnackBar(
-            content: Text('Přihlášení proběhlo úspěšně. Vítejte,' +
-                responseData['userName'] +
-                "!"),
-            duration: Duration(seconds: 2),
-          ),
-        );
-
-        Navigator.push(
-          context,
-          MaterialPageRoute(builder: (context) => LandingPage()),
-        );
-      } else {
-        // Handle null UserName or Password in response
-        ScaffoldMessenger.of(context).showSnackBar(
-          SnackBar(
-            content: Text('Přihlášení selhalo. Zkontrolujte své údaje.'),
-            duration: Duration(seconds: 2),
-          ),
-        );
-
-        print('Login failed. Null UserName or Password in response.');
-      }
-    } else {
-      // Handle login error, show error snackbar
+      // Registration successful, show success snackbar and navigate back to LandingPage
       ScaffoldMessenger.of(context).showSnackBar(
         SnackBar(
-          content: Text('Přihlášení selhalo. Zkontrolujte své údaje.'),
+          content: Text('Registrace proběhla úspěšně.'),
           duration: Duration(seconds: 2),
         ),
       );
 
-      print('Login failed. Status code: ${response.statusCode}');
+      Navigator.pop(context); // Navigate back to the landing page
+    } else {
+      // Registration failed, show error snackbar
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text('Registrace selhala. Zkontrolujte své údaje.'),
+          duration: Duration(seconds: 2),
+        ),
+      );
+
+      print('Registration failed. Status code: ${response.statusCode}');
     }
   }
 
@@ -94,9 +82,9 @@ class LoginPage extends StatelessWidget {
               ),
               SizedBox(height: 16.0),
               TextField(
-                controller: usernameController,
+                controller: customerNameController,
                 decoration: InputDecoration(
-                  labelText: 'Přihlašovací jméno',
+                  labelText: 'Jméno uživatele',
                   contentPadding: EdgeInsets.symmetric(horizontal: 16.0),
                 ),
               ),
@@ -110,9 +98,41 @@ class LoginPage extends StatelessWidget {
                 ),
               ),
               SizedBox(height: 16.0),
+              TextField(
+                controller: organizationController,
+                decoration: InputDecoration(
+                  labelText: 'Název organizace',
+                  contentPadding: EdgeInsets.symmetric(horizontal: 16.0),
+                ),
+              ),
+              SizedBox(height: 16.0),
+              TextField(
+                controller: emailController,
+                decoration: InputDecoration(
+                  labelText: 'E-mailová adresa',
+                  contentPadding: EdgeInsets.symmetric(horizontal: 16.0),
+                ),
+              ),
+              SizedBox(height: 16.0),
+              TextField(
+                controller: telNumberController,
+                decoration: InputDecoration(
+                  labelText: 'Telefonní číslo',
+                  contentPadding: EdgeInsets.symmetric(horizontal: 16.0),
+                ),
+              ),
+              SizedBox(height: 16.0),
+              TextField(
+                controller: usernameController,
+                decoration: InputDecoration(
+                  labelText: 'Username',
+                  contentPadding: EdgeInsets.symmetric(horizontal: 16.0),
+                ),
+              ),
+              SizedBox(height: 16.0),
               ElevatedButton(
                 onPressed: () {
-                  loginUser(context);
+                  registerUser(context);
                 },
                 style: ElevatedButton.styleFrom(
                   primary: Color(0xFFA3AE03),
@@ -123,28 +143,8 @@ class LoginPage extends StatelessWidget {
                   ),
                 ),
                 child: Text(
-                  'Přihlásit se',
+                  'Registrovat se',
                   style: TextStyle(fontSize: 18.0),
-                ),
-              ),
-              SizedBox(height: 16.0),
-              TextButton(
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => RegisterPage()),
-                  );
-                },
-                style: TextButton.styleFrom(
-                  padding: EdgeInsets.all(16.0),
-                ),
-                child: Text(
-                  'Registrovat',
-                  style: TextStyle(
-                    fontSize: 18.0,
-                    decoration: TextDecoration.underline,
-                    color: Color(0xFFA3AE03),
-                  ),
                 ),
               ),
             ],
